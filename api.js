fb08d7049aa739fd4889af3e7086fdee796bff5c
Sarthak Velapure <120236093+sarthak5644@users.noreply.github.com>
2026-07-19 14:26:38 +0530

    Update SCRIPT_URL definition in api.js

--- api.js
+++ api.js
@@ -19,7 +19,7 @@
    data (useful for designing the frontend before the sheet
    / script is wired up).
 ========================================================= */
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzMTVgxGwXK6OkV2wGjTtpHiaP2VqXV0TQqukODDNrwiZf38H2Z2s5BLTAR4RoN4vNw/exec"; // e.g. "https://script.google.com/macros/s/XXXXX/exec"
+ // e.g. "https://script.google.com/macros/s/XXXXX/exec"
 
 /* =========================================================
    API.JS — all communication with the Google Apps Script
@@ -42,6 +42,7 @@ const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzMTVgxGwXK6OkV2wGjT
    data (useful for designing the frontend before the sheet
    / script is wired up).
 ========================================================= */
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzMTVgxGwXK6OkV2wGjTtpHiaP2VqXV0TQqukODDNrwiZf38H2Z2s5BLTAR4RoN4vNw/exec";
 
 const API = (function () {
 
